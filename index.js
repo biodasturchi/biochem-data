4b149f06bfc58202a60ee9bb5cc9d6c81a6bf7a5
ilosrim <ilosrim@yahoo.com>
2022-07-01 23:25:20 +0500

    add cors package

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const articleData = require('./biochem.json')
+let cors = require('cors')
 
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send("Biochemstry labaratory")
